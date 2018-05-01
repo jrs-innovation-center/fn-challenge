26bc252195be21cbc011020a42bcba135236789e
tripott <tripott@gmail.com>
2018-05-01 14:13:59 -0400

    adding level 3 ex

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 <pre><code>
-  __              _           _ _                       
-/  _|_ __     ___| |__   __ _| | | ___ _ __   __ _  ___
-| |_| '_ \   / __| '_ \ / _` | | |/ _ | '_ \ / _` |/ _ \
-|  _| | | | | (__| | | | (_| | | |  __| | | | (_| |  __/
-|_| |_| |_|  \___|_| |_|\__,_|_|_|\___|_| |_|\__, |\___|
-                                             |___/                             
+    ____           __              ____                      __  _           
+   / __ \___  ____/ /_  ___  __   /  _/___  ________  ____  / /_(_)___  ____
+  / /_/ / _ \/ __  / / / / |/_/   / // __ \/ ___/ _ \/ __ \/ __/ / __ \/ __ \
+ / _, _/  __/ /_/ / /_/ />  <   _/ // / / / /__/  __/ /_/ / /_/ / /_/ / / / /
+/_/ |_|\___/\__,_/\__,_/_/|_|  /___/_/ /_/\___/\___/ .___/\__/_/\____/_/ /_/
+                                                  /_/                                          
 </code></pre>
 
 fn challenge is a project that helps you practice problem solving skills by writing functions. Sometimes you will be given hints. Other times you will have to learn a valuable skill: how to research. In other words, learn how to learn.
--- level3.js
+++ level3.js
@@ -15,8 +15,10 @@ import {
 
 export default function() {
   /* Level 2 - colors */
-  const ex1 = `complete the provided mapStateToProps function that takes in an object named "state"
-  and returns an object that contains the following property:
+  const ex1 = `A customer wishes to see cost and count totals for all items in their online shopping cart.
+  Prep the data for display before it is rendered onto the screen.  
+  Complete the provided mapStateToProps function that takes in an object named "state"
+  and returns an object that contains these 2 properties:
     1. totalCost - sum of the cost for all goods in cart using price multiplied by quantity
     2. cartCount - a count of all goods in cart using quantity`
   const exercise1 = state => {
@@ -34,12 +36,12 @@ export default function() {
     return mapStateToProps(state)
   }
 
-  const ex3 = `The exercise3 function will be passed an "action" object
+  const ex3 = `A customer has selected some items to purchase and needs to update their
+  shopping history application state. The exercise3 function will be passed an "action" object
     containing a property named "payload" whose value is an object representing
-    a single shopping history session. Using the checkoutDateTime value as a guide,
-    correctly update or append the customer's shopping history with the
-    action payload. Return the entire updated customer object.
-    Do not mutate the original customer object.`
+    a single shopping history session. Using the checkoutDateTime value as a way to indentify
+    an item in the shopping history, correctly update or append the customer's shopping history. 
+    Return the entire updated customer object. Do not mutate the original customer object.`
 
   const exercise3 = action => {
     const customer = {
@@ -56,7 +58,7 @@ export default function() {
         },
         {
           checkoutDateTime: '2018-02-22 12:05:34',
-          isComplete: false,
+          isComplete: true,
           storeNumber: 5982,
           cart: [
             { name: 'Fjallraven backpack', price: 59.99, quantity: 1 },
@@ -66,6 +68,21 @@ export default function() {
         }
       ]
     }
+    return null
+  }
+
+  const ex4 = `A welcome center app allows the user to select various interests and 
+  then taylors a tourist agenda based upon those interests.  
+
+  This exercise deals with toggling a user's selected interest from falst to true or true to false.  
+  The exercise4 function will be passed an "action" object
+    containing a property named "payload" whose value is an object representing
+    a selected activity.  Use the payload to identify and toggle the appropriate 
+    interest within the interests array. Return a new array.  
+    Do not mutate the original array.`
+
+  const exercise4 = action => {
+    const interests = [{ name: 'Soccer', selected: false }]
     return null
   }
 
@@ -132,7 +149,7 @@ export default function() {
         shoppingHistory: [
           {
             checkoutDateTime: '2018-03-05 16:02:01',
-            isComplete: false,
+            isComplete: true,
             storeNumber: 5982,
             cart: [
               { name: 'Ironic Hipster T-shirt', price: 19.99, quantity: 1 },
@@ -141,7 +158,7 @@ export default function() {
           },
           {
             checkoutDateTime: '2018-02-22 12:05:34',
-            isComplete: false,
+            isComplete: true,
             storeNumber: 5982,
             cart: [
               { name: 'Fjallraven backpack', price: 59.99, quantity: 1 },
